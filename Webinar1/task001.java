8dfdcb5481bf20e3155af7943752aadb2d36118d
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-04-17 23:46:27 +0300

    Webinar1 starting

---
+++ Webinar1/task001.java
@@ -0,0 +1,33 @@
+/* 
+В консоли запросить имя пользователя. 
+В зависимости от текущего времени, вывести приветствие вида
+"Доброе утро, <Имя>!", если время от 05:00 до 11:59
+"Добрый день, <Имя>!", если время от 12:00 до 17:59;
+"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+ */
+
+package Webinar1;
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class task001 {
+    public static void main(String[] args) {
+        System.out.println("Введите Ваше имя: ");
+        Scanner scanner = new Scanner(System.in, "cp866");
+        String name = scanner.nextLine();
+        LocalTime time = LocalTime.now();
+        // LocalTime time1 = LocalTime.of(11, 55, 0);
+        int hour = time.getHour();
+        if (hour >= 5 && hour < 12) {
+            System.out.println("Доброе утро," + name + "!");
+        } else if (hour >= 12 && hour < 18) {
+            System.out.println("Добрый день," + name + "!");
+        } else if (hour >= 18 && hour < 23) {
+            System.out.println("Добрый вечер," + name + "!");
+        } else {
+            System.out.println("Доброй ночи, " + name + "!");
+        }
+    }
+}
